c1918f0ff5a49c935322f867ebfcc136928c4ae8
Mileo210 <miguel.santos@mutantbr.com>
2019-10-09 19:39:22 -0300

    REST Testado!

--- src/main/java/com/miguel/cursomc/CursomcApplication.java
+++ src/main/java/com/miguel/cursomc/CursomcApplication.java
@@ -1,9 +1,14 @@
 package com.miguel.cursomc;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan("com.miguel.cursomc.resources")
 public class CursomcApplication {
 
 	public static void main(String[] args) {
